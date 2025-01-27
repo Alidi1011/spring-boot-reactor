efefbfeafa0b205df6e06e978d20a249c54cc3ec
Alisson <arteaga10alisson@gmail.com>
2025-01-27 14:35:59 -0500

    add operators first part

--- src/main/java/com/aarteaga/springboot/reactor/app/SpringBootReactorApplication.java
+++ src/main/java/com/aarteaga/springboot/reactor/app/SpringBootReactorApplication.java
@@ -1,5 +1,8 @@
 package com.aarteaga.springboot.reactor.app;
 
+import com.aarteaga.springboot.reactor.app.models.Usuario;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +11,8 @@ import reactor.core.publisher.Flux;
 
 @SpringBootApplication
 public class SpringBootReactorApplication implements CommandLineRunner{
+	
+	private static final Logger log = LoggerFactory.getLogger(SpringBootReactorApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootReactorApplication.class, args);
@@ -16,16 +21,37 @@ public class SpringBootReactorApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		//Es un observable
-		Flux<String> nombres = Flux.just("Roxana", "Amelia", "Diego", "Sebastian")
-				//.doOnNext(elemento -> System.out.println(elemento));
-				.doOnNext(System.out::println);
+		Flux<Usuario> nombres = Flux.just("Roxana Guzmán", "Amelia Fulano", "Alexa Sultano", "Diego Almaro", "Sebastian Llosa", "Bruce Lee", "Bruce Willis")
+				.map(nombre -> new Usuario(nombre.split(" ")[1], nombre.split(" ")[0].toUpperCase()))
+				.filter(usuario -> usuario.getNombre().toLowerCase().equals("bruce"))
+				.doOnNext(usuario -> {
+					if(usuario == null){
+						throw new RuntimeException("Nombre no pueden ser vacíos");
+					}
+					System.out.println(usuario.getNombre().concat(" ").concat(usuario.getApellido()));
+				})
+				//.doOnNext(System.out::println);
+				.map(usuario -> {
+					String nombre = usuario.getNombre().toLowerCase();
+					usuario.setNombre(nombre);
+					return usuario;
+				});
 
 		
-		nombres.subscribe();
-		
-				
-		
-		
+		//nombres.subscribe(log::info);
+		nombres.subscribe(e -> log.info(e.toString()),
+				error -> log.error(error.getMessage()),
+				new Runnable() {
+					@Override
+					public void run() {
+						log.info("Ha finalizado la ejecución del observable con éxito!");
+					}
+				});
+
+
+
+
+
 	}
 
 }
---
+++ src/main/java/com/aarteaga/springboot/reactor/app/models/Usuario.java
@@ -0,0 +1,35 @@
+package com.aarteaga.springboot.reactor.app.models;
+
+public class Usuario {
+    private String nombre;
+    private String apellido;
+
+    public Usuario(String apellido, String nombre) {
+        this.apellido = apellido;
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                '}';
+    }
+}
